ffb1f776750baa5ab77a2bf0bab974bc1b03c342
hashfunc <root@hashfunc.io>
2023-05-29 22:14:47 +0900

    feat(programmers): Add `86971`

---
+++ programmers/86971/test.cpp
@@ -0,0 +1,25 @@
+#include <gtest/gtest.h>
+
+#include "solution.hpp"
+
+#define NAME 86971
+
+class TestParameters : public ::testing::TestWithParam<std::tuple<int, std::vector<std::vector<int>>, int>> {};
+
+TEST_P(TestParameters, NAME) {
+  const auto &n = std::get<0>(GetParam());
+  const auto &wires = std::get<1>(GetParam());
+  const auto &expected = std::get<2>(GetParam());
+
+  EXPECT_EQ(solution(n, wires), expected);
+}
+
+INSTANTIATE_TEST_SUITE_P(
+    NAME,
+    TestParameters,
+    ::testing::Values(
+        std::make_tuple(9, std::vector<std::vector<int>>{{1, 3}, {2, 3}, {3, 4}, {4, 5}, {4, 6}, {4, 7}, {7, 8}, {7, 9}}, 3),
+        std::make_tuple(4, std::vector<std::vector<int>>{{1, 2}, {2, 3}, {3, 4}}, 0),
+        std::make_tuple(7, std::vector<std::vector<int>>{{1, 2}, {2, 7}, {3, 7}, {3, 4}, {4, 5}, {6, 7}}, 1)
+    )
+);
