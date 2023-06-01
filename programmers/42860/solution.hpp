27afc7326539ebb5f38151151c2e90e7950cbbf1
hashfunc <root@hashfunc.io>
2023-06-01 23:55:41 +0900

    feat(programmers): Add `42860`

---
+++ programmers/42860/solution.hpp
@@ -0,0 +1,25 @@
+#include <algorithm>
+#include <string>
+#include <vector>
+
+int solution(std::string name) {
+  int length = static_cast<int>(name.length());
+  int move = length - 1;
+  int count = 0;
+
+  for (int index = 0; index < length; index++) {
+	count += std::min(name[index] - 'A', 'Z' - name[index] + 1);
+
+	int offset = 1;
+	for (; index + offset < length; ++offset) {
+	  if (name[index + offset] != 'A') {
+		break;
+	  }
+	}
+
+	move = std::min(move, index * 2 + length - (index + offset));
+	move = std::min(move, (length - (index + offset)) * 2 + index);
+  }
+
+  return count + move;
+}
---
+++ programmers/42860/test.cpp
@@ -0,0 +1,22 @@
+#include <gtest/gtest.h>
+
+#include "solution.hpp"
+
+#define NAME 42860
+
+class TestParameters : public ::testing::TestWithParam<std::tuple<std::string, int>> {};
+
+TEST_P(TestParameters, NAME) {
+  const auto &[name, expected] = GetParam();
+
+  EXPECT_EQ(solution(name), expected);
+}
+
+INSTANTIATE_TEST_SUITE_P(
+	NAME,
+	TestParameters,
+	::testing::Values(
+		std::make_tuple("JEROEN", 56),
+		std::make_tuple("JAN", 23)
+	)
+);
