13e99c7373caa0e82d0a9663bbf9a94de1634cb3
hashfunc <root@hashfunc.io>
2023-06-03 21:15:21 +0900

    feat(programmers): Add `178871`

---
+++ programmers/178871/test.cpp
@@ -0,0 +1,26 @@
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+#include "solution.hpp"
+
+#define NAME 12906
+
+class TestParameters : public ::testing::TestWithParam<std::tuple<std::vector<std::string>, std::vector<std::string>, std::vector<std::string>>> {};
+
+TEST_P(TestParameters, NAME) {
+  const auto &[players, callings, result] = GetParam();
+
+  ASSERT_THAT(solution(players, callings), testing::ElementsAreArray(result));
+}
+
+INSTANTIATE_TEST_SUITE_P(
+    NAME,
+    TestParameters,
+    ::testing::Values(
+        std::make_tuple(
+			std::vector<std::string>{"mumu", "soe", "poe", "kai", "mine"},
+			std::vector<std::string>{"kai", "kai", "mine", "mine"},
+			std::vector<std::string>{"mumu", "kai", "mine", "soe", "poe"}
+		)
+    )
+);
--- programmers/README.md
+++ programmers/README.md
@@ -29,3 +29,4 @@
 | [86971 - 전력망을 둘로 나누기](./86971) | [🔗](https://school.programmers.co.kr/learn/courses/30/lessons/86971)  |
 | [87946 - 피로도](./87946)         | [🔗](https://school.programmers.co.kr/learn/courses/30/lessons/87946)  |
 | [155652 - 둘만 암호](./155652)     | [🔗](https://school.programmers.co.kr/learn/courses/30/lessons/155652) |
+| [178871 - 달기기 경주](./178871)    | [🔗](https://school.programmers.co.kr/learn/courses/30/lessons/178871) |
