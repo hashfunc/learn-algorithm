f9f5dc4e96dd4b06325370516e555cc180d7e9e2
hashfunc <root@hashfunc.io>
2023-06-14 21:52:54 +0900

    refactor(programmers): Refactor `42885`

--- programmers/42885/solution.hpp
+++ programmers/42885/solution.hpp
@@ -1,45 +1,16 @@
-#include <map>
+#include <algorithm>
 #include <vector>
 
 int solution(std::vector<int> people, int limit) {
-  auto weight_map = std::map<int, int>();
+  std::sort(people.rbegin(), people.rend());
 
-  for (auto weight : people) {
-    if (weight_map.find(weight) == weight_map.end()) {
-      weight_map.insert({weight, 1});
-    } else {
-      weight_map[weight] += 1;
+  int head = 0;
+
+  for (auto tail = people.size() - 1; head <= tail; ++head) {
+    if (people[head] + people[tail] <= limit) {
+      --tail;
     }
   }
 
-  int answer = 0;
-
-  for (auto r_iter = weight_map.rbegin(); r_iter != weight_map.rend(); ++r_iter) {
-    auto &[r_key, r_value] = *r_iter;
-
-    while (r_value > 0) {
-      --r_value;
-
-      int weight = r_key;
-
-      for (auto iter = weight_map.begin(); iter->first <= r_iter->first; ++iter) {
-        auto &[key, value] = *iter;
-
-        if (value == 0) {
-          continue;
-        }
-
-        if (weight + key <= limit) {
-          --value;
-          weight += key;
-        } else {
-          break;
-        }
-      }
-
-      ++answer;
-    }
-  }
-
-  return answer;
+  return head;
 }
