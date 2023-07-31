a19772d7e177dbe86a34bbc21b127921040b4492
hashfunc <root@hashfunc.io>
2023-07-31 23:03:14 +0900

    feat(programmers): Add `181951`

---
+++ programmers/lv0/181951/solution.hpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+void solution(const int a, const int b)
+{
+    std::cout << "a = " << a << std::endl;
+    std::cout << "b = " << b << std::endl;
+}
+
+int main()
+{
+    int a, b;
+
+    std::cin >> a >> b;
+
+    solution(a, b);
+
+    return 0;
+}
---
+++ programmers/lv0/181951/test.cpp
@@ -0,0 +1,21 @@
+#include <gtest/gtest.h>
+
+#include "solution.hpp"
+
+using namespace std::string_literals;
+
+TEST(TEST_181951, TEST_1)
+{
+    int a = 4;
+    int b = 5;
+
+    const auto expected = "a = 4\nb = 5"s;
+
+    testing::internal::CaptureStdout();
+
+    solution(a, b);
+
+    const auto &result = testing::internal::GetCapturedStdout();
+
+    EXPECT_EQ(expected, str);
+}
